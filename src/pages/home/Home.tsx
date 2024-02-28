6523c6d685bb784ae792c86218eb115bcc104268
johnnataa <johnnata_@outlook.com>
2024-02-28 12:59:53 -0300

    Rotas e e ajuste no botão/ link Ver Produtos

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import homeLogo from '../../assets/img/home.jpg'
 import ListaCategoria from '../../components/categorias/listaCategoria/ListaCategoria';
 
@@ -12,7 +13,7 @@ function Home() {
   
               <div className="flex justify-around gap-4">
               
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver produtos</button>
+                <Link to='/produtos' className='rounded bg-white text-blue-800 py-2 px-4'>Ver produtos</Link>
               </div>
             </div>
   
